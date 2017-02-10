7d972909fda36fec71f626c431500a30ea4a23a7
Kurt McGallion <kurt.mcgallion@gmail.com>
2017-02-10 11:32:13 -0600

    finsihed js-basics assessment

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -5,7 +5,9 @@ var name = 'Tyler';
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
-
+function isTyler(name) {
+  return  (name==="Tyler" );
+}
 //////////////////PROBLEM 2////////////////////
 
 
@@ -13,7 +15,9 @@ var name = 'Tyler';
 
 
   //Code Here
-
+function getName() {
+  return prompt("please enter your name");
+}
 
 //////////////////PROBLEM 3////////////////////
 
@@ -23,7 +27,9 @@ var name = 'Tyler';
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
-
+function welcome() {
+  alert("Welcome, "+getName());
+}
 
 //////////////////PROBLEM 4////////////////////
 
@@ -32,7 +38,7 @@ var name = 'Tyler';
 
 //What is the difference between arguments and parameters?
 
-  //Answer Here
+  //A parameter is a variable in a function, an argument is the data you pass into the parameters.
 
 
 //////////////////PROBLEM 5////////////////////
@@ -42,7 +48,7 @@ var name = 'Tyler';
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
 
-  //Answer Here
+  //0, null, undefined, false, Nan, ""  -  if(var)
 
 
 
@@ -53,15 +59,17 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
-  
+function myName() {
+  return "Kurt";
+}
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
   //Code Here
-
+var newMyName = myName;
 //Now alert the result of invoking newMyName
-
+alert(newMyName());
 
 
 //////////////////PROBLEM 7////////////////////
@@ -71,9 +79,14 @@ var name = 'Tyler';
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
-
+function outerFn(){
+  return function (){
+    return "Kurt";
+  };
+}
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
-
+var innerFn = outerFn();
 //Now invoke innerFn.
+innerFn();
